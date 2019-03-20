a079f33c930f2eda9df77f535274e2152b2717e9
Phosky <dioxyns@hotmail.com>
2019-03-20 11:04:34 +0100

    Apartado b puestas las imagenes y la animacion completa

--- app/src/main/java/com/example/oscar/homersipson_oscargonzalez/MainActivity.java
+++ app/src/main/java/com/example/oscar/homersipson_oscargonzalez/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.oscar.homersipson_oscargonzalez;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private AnimationDrawable anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        animation();
     }
+
+    private void animation() {
+        ImageView img = (ImageView)findViewById(R.id.simple_anim);
+        anim = (AnimationDrawable)img.getDrawable();
+        img.post(run);
+    }
+    Runnable run = new Runnable() {
+        @Override
+        public void run() {
+            anim.start();
+        }
+    };
 }
+
